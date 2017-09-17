d6e3391d4509a7006f6a99837bab4b606fcd6606
Bartłomiej Jakubowski <bartlomiej-jakubowski@wp.pl>
2017-09-17 19:19:27 +0200

    Add files via upload
    Add colors to info

--- OSInfo.js
+++ OSInfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 var TimeModule = require('./TimeModule');
 
 function getOSinfo() {
@@ -12,12 +13,12 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
+    console.log('System:'.grey, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.blue, cpu);
     console.log(TimeModule.print()); 
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log('User name:'.yellow, userInfo.username);
+    console.log('Home dir:'.gray, userInfo.homedir);
 }
 
 exports.print = getOSinfo;
