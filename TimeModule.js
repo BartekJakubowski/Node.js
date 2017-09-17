823d2efd1edc123fa57fcd253807b1e0311a82f4
Bartłomiej Jakubowski <bartlomiej-jakubowski@wp.pl>
2017-09-17 19:18:19 +0200

    Add files via upload
    Added colors to info.

--- TimeModule.js
+++ TimeModule.js
@@ -7,7 +7,7 @@ var minuty = Math.floor((uptime - godziny * 3600)/60);
 var sekundyReszta = (sekundy - godziny * 3600 - minuty * 60).toFixed(0);
 
 function TimeModule() {
-    console.log('Uptime: ~', godziny, 'godz. ', minuty, 'min. ', sekundyReszta, 'sek.',);
+    console.log('Uptime: ~'.green, godziny, 'godz. ', minuty, 'min. ', sekundyReszta, 'sek.');
 };
 
 exports.print = TimeModule;
