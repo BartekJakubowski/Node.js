00547f90b5dde6fbdd41a652141de04507e27244
Bartłomiej Jakubowski <bartlomiej-jakubowski@wp.pl>
2017-09-17 18:37:03 +0200

    Add files via upload
    Time module

---
+++ TimeModule.js
@@ -0,0 +1,13 @@
+var os = require('os');
+var uptime = os.uptime();
+
+var sekundy = uptime.toFixed(0);
+var godziny = Math.floor(uptime / 3600);
+var minuty = Math.floor((uptime - godziny * 3600)/60); 
+var sekundyReszta = (sekundy - godziny * 3600 - minuty * 60).toFixed(0);
+
+function TimeModule() {
+    console.log('Uptime: ~', godziny, 'godz. ', minuty, 'min. ', sekundyReszta, 'sek.',);
+};
+
+exports.print = TimeModule;
