08625213474c1a6bad4e192e5fbe491715b19432
Bartłomiej Jakubowski <bartlomiej-jakubowski@wp.pl>
2017-09-18 20:51:39 +0200

    Update TimeModule.js
    Function changed

--- TimeModule.js
+++ TimeModule.js
@@ -1,13 +1,14 @@
 var os = require('os');
 var uptime = os.uptime();
 
-var sekundy = uptime.toFixed(0);
-var godziny = Math.floor(uptime / 3600);
-var minuty = Math.floor((uptime - godziny * 3600)/60); 
-var sekundyReszta = (sekundy - godziny * 3600 - minuty * 60).toFixed(0);
-
 function TimeModule() {
-    console.log('Uptime: ~'.green, godziny, 'godz. ', minuty, 'min. ', sekundyReszta, 'sek.');
+    var sekundy = uptime.toFixed(0);
+    var godziny = Math.floor(uptime / 3600);
+    var minuty = Math.floor((uptime - godziny * 3600)/60); 
+    var sekundyReszta = (sekundy - godziny * 3600 - minuty * 60).toFixed(0);
+    var uptimeTime = console.log('Uptime: ~'.green, godziny, 'godz. ', minuty, 'min. ', sekundyReszta , 'sek.')
+    
+    return uptimeTime;
 };
 
-exports.print = TimeModule;
+exports.print = getTimeModule;
